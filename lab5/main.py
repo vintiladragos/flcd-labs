f26a85e26175cafaaa97478f69bcde2a80bc5238
dragosvintila <dragos7v@yahoo.com>
2023-11-27 18:21:59 +0200

    grammar cleaned up

---
+++ lab5/main.py
@@ -0,0 +1,15 @@
+from grammar import Grammar
+
+if __name__ == '__main__':
+    filename = "g2.txt"
+    grammar = Grammar(filename)
+
+    print(grammar)
+
+    if grammar.cfg_check():
+        print("The grammar is a Context-Free Grammar (CFG).\n")
+    else:
+        print("The grammar is not a Context-Free Grammar (CFG).\n")
+
+    non_terminal = "<statement>"
+    print(f"Productions of the non-terminal {non_terminal}:\n\t{grammar.productions_for_non_terminal(non_terminal)}")
